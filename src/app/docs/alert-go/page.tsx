733f9b43a7e9fe81fadb9d592cb37dade4c84fb1
AsimMahmudov <mahmudovasim799@gmail.com>
2025-12-25 13:38:40 +0600

    dev

--- src/app/docs/alert-go/page.tsx
+++ src/app/docs/alert-go/page.tsx
@@ -1,5 +1,13 @@
 import AlertGo from '@/components/pages/docs/alert-go/AlertGo';
-import React from 'react';
+import { generateMetadata } from '@/lib/seo';
+ 
+
+export const metadata = generateMetadata({
+  title: "Alert-Go",
+  description: "Лёгкая и простая библиотека уведомлений для веба, аналог Sonner. Поддерживает все позиции (top-left, top-center, bottom-right и т.д.), анимации, действия и TypeScript. Работает с React, Next.js, Vite и любыми сборщиками.",
+  url: "https://next-structure-seven.vercel.app/",
+  image: "/alim.jpg",
+});
 
 const AlertGoPage = () => <AlertGo/>
 
--- src/app/docs/phone-input/page.tsx
+++ src/app/docs/phone-input/page.tsx
@@ -1,5 +1,13 @@
 import PhoneInput from "@/components/pages/docs/phone-input/PhoneInput";
-import React from "react";
+import { generateMetadata } from "@/lib/seo";
+
+
+export const metadata = generateMetadata({
+  title: "Phone-Go",
+  description: "Мощный и простой React-компонент для ввода телефонных номеров с современным UI, поддержкой всех стран, масками, валидацией и кастомизацией. Работает с TypeScript, Next.js, Vite и любыми сборщиками.",
+  url: "https://next-structure-seven.vercel.app/",
+  image: "/alim.jpg",
+});
 
 const page = () => <PhoneInput />;
 
